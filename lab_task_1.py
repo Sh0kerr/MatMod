3d5e1616383170152ff39a3629d17e0cf04aea80
Sh0kerr <61948294+Sh0kerr@users.noreply.github.com>
2022-11-16 12:04:38 +0300

    Update lab_task_1.py

--- lab_task_1.py
+++ lab_task_1.py
@@ -5,12 +5,13 @@ import numpy as np
 
 # Defining a function f(x, y)
 def f(sumval1, sumval2):
-    return (x[i] + sumval1) * math.exp(-((x[i] + sumval1) ** 2)) - 2 * (x[i] + sumval1) * (y1[i] + sumval2)
+	return (x[i] + sumval1) * math.exp(-((x[i] + sumval1) ** 2)) - 2 * (x[i] + sumval1) * (y2[i] + sumval2)
 
 
 # Creating list of values from 0 to 1 with constant relative distance 0,05
 h = 0.05
-x = np.linspace(0, 1, int(1 / h))
+split_num = int(1 / h) + 1
+x = np.linspace(0, 1, split_num)
 
 # Creating Euler, Runge-Kutta and analytic lists of values
 # Defining their initial conditions
@@ -25,27 +26,27 @@ y3[0] = 1
 
 # Filling the lists with values
 for i in range(len(x) - 1):
-    # Euler
-    y1[i + 1] = y1[i] + h * f(0, 0)
+	# Euler
+	y1[i + 1] = y1[i] + h * (x[i] * math.exp(-(x[i] ** 2)) - 2 * x[i] * y1[i])
 
-    # Runge-Kutta 4 orders
-    k1 = h * f(0, 0)
-    k2 = h * f(h / 2, k1 / 2)
-    k3 = h * f(h / 2, k2 / 2)
-    k4 = h * f(h, k3)
-    y2[i + 1] = y2[i] + (k1 + 2 * k2 + 2 * k3 + k4) / 6
+	# Runge-Kutta 4 orders
+	k1 = h * f(0, 0)
+	k2 = h * f(h / 2, k1 / 2)
+	k3 = h * f(h / 2, k2 / 2)
+	k4 = h * f(h, k3)
+	y2[i + 1] = y2[i] + (k1 + 2 * k2 + 2 * k3 + k4) / 6
 
-    # Analytics
-    y3[i + 1] = (x[i + 1] ** 2 + 2) / (2 * math.exp(x[i + 1] ** 2))
+	# Analytics
+	y3[i + 1] = (x[i + 1] ** 2 + 2) / (2 * math.exp(x[i + 1] ** 2))
 
 # Printing all y lists in format: y1 y2 y3 with precision up to 4 digits
 print('{:<10} {:<10} {:<10} {:<10}'.format('x', 'Euler', 'R-K', 'Analytic'))
 for i in range(len(x)):
-    x_i = "%.4f" % x[i]
-    y_euler = "%.4f" % y1[i]
-    y_rk = "%.4f" % y2[i]
-    y_analytic = "%.4f" % y3[i]
-    print('{:<10} {:<10} {:<10} {:<10}'.format(x_i, y_euler, y_rk, y_analytic))
+	x_i = "%.2f" % x[i]
+	y_euler = "%.4f" % y1[i]
+	y_rk = y2[i]
+	y_analytic = y3[i]
+	print('{:<10} {:<10} {:<10} {:<10}'.format(x_i, y_euler, y_rk, y_analytic))
 
 print('\n', '\n')
 
@@ -63,7 +64,7 @@ R-K error = {abs(float(y2[-1]) - float(y3[-1]))}.''')
 
 # Plotting
 plt.plot(x, y1, label="Euler")
-plt.plot(x, y2, label="Runge-Kutta 4 orders")
-plt.plot(x, y3, label="Analytics")
+plt.plot(x, y2, label="Runge-Kutta 4 orders", linestyle='--')
+plt.plot(x, y3, label="Analytics", linestyle=':')
 plt.legend()
 plt.show()
